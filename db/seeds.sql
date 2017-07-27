046e0f404373d620261d5352f66c5eaf73e3e7d1
Jose Salazar <jose.salazar.home@gmail.com>
2017-07-26 19:01:42 -0700

    Deviant Branch Complete

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,9 +4,3 @@ INSERT INTO burgers (name) VALUES ('Chicken Sandwich');
 INSERT INTO burgers (name) VALUES ('Big Mac');
 INSERT INTO burgers (name) VALUES ('Quarter Pounder with Cheese');
 INSERT INTO burgers (name) VALUES ('Royale with Cheese');
-
-INSERT INTO eatenBurgers (name) VALUES ('Rally\'s Burger');
-
--- MERGES (DO NOT RUN)
-
-INSERT INTO eatenBurgers SELECT * FROM burgers WHERE burgers.id = 6 DELETE FROM burgers WHERE burgers.id = 6;
